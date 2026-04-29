672548f657d58710fd6a9ce613bc67a8613c19b6
Alejandro Marquez <alejandromarquez.alba@gmail.com>
2026-04-29 04:40:20 -0300

    feat: setup database schema with Prisma and pgvector

---
+++ prisma/migrations/20260429073803_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Document" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "embedding" vector,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
