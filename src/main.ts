c03cb5ea48093d616f4f1765f0025a61053985d1
Alejandro Marquez <alejandromarquez.alba@gmail.com>
2026-06-04 17:56:52 -0300

    fix: habilitar CORS para frontend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Habilitar CORS para el frontend
+  app.enableCors({
+    origin: ['https://front-rag.vercel.app', 'http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
